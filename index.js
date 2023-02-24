a4641f071f995d12284d721790f4d4f81f7f82f2
Thiago <thiagoalves060@gmail.com>
2023-02-24 06:09:52 -0300

    consulta de alunos tratamento de erro

--- index.js
+++ index.js
@@ -14,17 +14,30 @@ exports.handler = (event, context, callback) => {
 
   // Consulta SQL para buscar informações de alunos na tabela de alunos
   connection.query('SELECT * FROM alunos', function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-    // Fechar a conexão com o banco de dados MySQL
-    connection.end();
-    // Transformar os resultados em formato JSON
-    const jsonResults = JSON.stringify(results);
-    // Retornar os resultados da consulta em formato JSON com o status code 200
-    const response = {
-      statusCode: 200,
-      body: jsonResults
-    };
-    callback(null, response);
+    if (error) {
+      console.log(error);
+        // Fechar a conexão com o banco de dados MySQL
+        connection.end();
+        // Retornar um status code 400 e informações adicionais sobre o erro
+        const response = {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Ocorreu um erro ao consultar alunos.' })
+        };
+        callback(null, response);
+      }
+    else {
+      console.log(results);
+      // Fechar a conexão com o banco de dados MySQL
+      connection.end();
+      // Transformar os resultados em formato JSON
+      const jsonResults = JSON.stringify(results);
+      // Retornar os resultados da consulta em formato JSON com o status code 200
+      const response = {
+        statusCode: 200,
+        body: jsonResults
+      };
+      callback(null, response);
+    }
+    
   });
 };
